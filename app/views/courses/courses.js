eccb2a4c9268f352d73e2d752152f802c61aea75
Elena Lambova <elena.g.lambova@gmail.com>
2015-09-16 18:56:09 +0300

    Adding to edit controller courses his logic. Change courses.html to accept get param.

--- app/views/courses/courses.js
+++ app/views/courses/courses.js
@@ -28,15 +28,29 @@
     app.controller('CourseController', function (UrlResource) {
         var controller = this;
         UrlResource.query({pageUrl: 'courses'}, function (data) {
-            controller.courses = data;
+            var courses = {};
+            angular.forEach(data, function (course) {
+                courses[course.id] = course;
+            });
+            controller.courses = courses;
         });
     });
-    app.controller('CoursesAddController', function ($scope, $location) {
+    app.controller('CoursesAddController', function (UrlResource, $scope, $location) {
         $scope.reset = function () {
             $location.path('/courses');
         };
     });
-    app.controller('CoursesEditController', function ($scope, $location) {
+    app.controller('CoursesEditController', function (UrlResource, $scope, $location) {
+        var controller = this;
+        UrlResource.query({pageUrl: 'courses'}, function (data) {
+            var courses = {};
+            angular.forEach(data, function (course) {
+                if ($location.search().id === course.id.toString()) {
+                    courses[course.id] = course;
+                }
+            });
+            controller.courses = courses;
+        });
         $scope.reset = function () {
             $location.path("/courses");
         };
